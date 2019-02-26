81d1468b5bbfaaa9ef5a3a857531de44852df28c
Matthew Rieger <matthewcreanrieger@gmail.com>
2019-02-25 19:23:53 -0500

    Initial Commit

---
+++ Spor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spor
+//
+//  Created by Matthew Rieger on 2/25/19.
+//  Copyright © 2019 Escargot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
